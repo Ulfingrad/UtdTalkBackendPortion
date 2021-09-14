b1cc0ef46b08bdc205b13d368030932bc00ee7dd
Austin Slobodnik <austinslobodnik@gmail.com>
2021-09-14 16:21:16 -0500

    minorChanges

--- src/todoItems.ts
+++ src/todoItems.ts
@@ -2,24 +2,25 @@ import express from 'express';
 
 export const todoRouter = express.Router();
 
-const todoList: string[] = [];
+const todoList: { id: string}[] = [];
 
-todoRouter.get('', (req, res) => {
+todoRouter.get("", (req, res) => {
     res.send(todoList);
 });
 
-todoRouter.post('', (req, res) => {
+todoRouter.post("", (req, res) => {
     const { todoItem } = req.body;
     todoList.push(todoItem);
-    res.send(todoList);
+    res.send(todoItem);
 });
 
-todoRouter.delete('', (req, res) => {
-  const { todoItem } = req.body;
-  if(todoList.indexOf(todoItem as string) === -1) {
-    res.status(400).send('ERR: todo item must exist to be deleted');
-    return;
-  }
-  todoList.splice(todoList.indexOf(todoItem as string), 1);
-  res.send(todoList);
-})
+todoRouter.delete(':id', (req, res) => {
+    const { id } = req.params;
+    const index = todoList.findIndex(todoItem => todoItem.id === id);
+    if(index === -1) {
+      res.status(400).send('ERR: todo item must exist to be deleted');
+      return;
+    }
+    todoList.splice(index, 1);
+    res.send(todoList);
+  })
