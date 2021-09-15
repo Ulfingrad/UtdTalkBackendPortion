7cc23ffb1311329cade811450b0d45ac9fc95457
Austin Slobodnik <austinslobodnik@gmail.com>
2021-09-15 10:41:56 -0500

    comments

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import express from "express";
+import cors from "cors";
 import { todoRouter } from "./todoItems";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // Lets the app know that it will potentially be receiving JSON objects
+app.use(cors()); // Necessary in order to interact with the front end
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.get("", (req, res) => { // simple example of a 'get' endpoint, as we are getting back the string 'Hello world!'
+    res.send("Hello world!");
+})
 
-app.use('/todo', todoRouter);
+app.use("/todo", todoRouter); // Adds the /todo route
 
-app.use((req, res) => {
-	res.status(404).send('Uh oh, looks like you messed up!');
+app.use((req, res) => { // Any route not specified will result in a 404 (not found)
+    res.status(404).send('you done goofed');
 });
 
 const port = 4000;
-app.listen(port);
+app.listen(port); // Activates the app on port 4000
 console.log(`App listening on port ${port}`);
--- src/todoItems.ts
+++ src/todoItems.ts
@@ -1,29 +1,29 @@
 import express from 'express';
 
-export const todoRouter = express.Router();
+export const todoRouter = express.Router(); // Creates and exports the router for the todo route
 
-const todoList: { id: string, contents: string}[] = [];
+const todoList: { id: string, contents: string}[] = []; // Holds the list of todo items. Each todo item MUST be an object with an 'id' and 'contents' attribute, which both MUST be of type 'string'
 let currentId = 0;
 
-todoRouter.get("", (req, res) => {
+todoRouter.get("", (req, res) => { // Returns the list of todo items
     res.send(todoList);
 });
 
-todoRouter.post("", (req, res) => {
+todoRouter.post("", (req, res) => { // Adds a todo item to the todoList where the id is the currentId and the contents is the received todoItem
     const { todoItem } = req.body;
     const newItem = {id: currentId.toString(), contents: todoItem};
     todoList.push(newItem);
-    currentId++;
+    currentId++; // Increments the currentId variable so that each todo item has a unique id
     res.send(newItem);
 });
 
-todoRouter.delete('/:id', (req, res) => {
+todoRouter.delete('/:id', (req, res) => { // Looks for an id to delete based on the route parameters (i.e. in http://localhost:4000/todo/3, 3 would be the route parameter)
     const { id } = req.params;
-    const index = todoList.findIndex(todoItem => todoItem.id === id);
-    if(index === -1) {
+    const index = todoList.findIndex(todoItem => todoItem.id === id); // Will try to find a todo item with the corresponding received id and will return a -1 if no respective id is found
+    if(index === -1) { // Any id not found will result in a 400 error (bad request)
       res.status(400).send('ERR: todo item must exist to be deleted');
       return;
     }
-    todoList.splice(index, 1);
+    todoList.splice(index, 1); // Splices (removes) the element with the specified id
     res.send(200);
   })
